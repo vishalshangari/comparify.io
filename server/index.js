c0839554c30f558440b12d7c4b888a46c13141aa
blackmambabryant <vishal.shangari@gmail.com>
2020-08-06 11:28:34 -0700

    set up basic spotify auth api call

--- server/index.js
+++ server/index.js
@@ -1,11 +1,37 @@
 const express = require("express");
 const path = require("path");
 const cluster = require("cluster");
+const queryString = require("query-string");
+const cookieParser = require("cookie-parser");
 const numCPUs = require("os").cpus().length;
 require("dotenv").config();
 
 const isDev = process.env.NODE_ENV !== "production";
 const port = process.env.PORT || 3001;
+
+// Spotify API config
+const client_id = "70f7c0f3f8a04350b0957f010488e475";
+const client_secret = "a832c80320fc4450a0fa1dbc1465d3c0";
+const redirect_uri = "http://localhost:3001/api/callback";
+const spotify_auth_url = "https://accounts.spotify.com/authorize?";
+
+/**
+ * Generates a random string containing numbers and letters
+ * @param  {number} length The length of the string
+ * @return {string} The generated string
+ */
+const generateRandomString = (length) => {
+  var text = "";
+  const possible =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+  for (let i = 0; i < length; i++) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  return text;
+};
+
+const stateKey = "spotify_auth_state";
 
 // Multi-process to utilize all CPU cores.
 if (!isDev && cluster.isMaster) {
@@ -23,6 +49,7 @@ if (!isDev && cluster.isMaster) {
   });
 } else {
   const app = express();
+  app.use(cookieParser());
 
   // Priority serve any static files.
   app.use(express.static(path.resolve(__dirname, "../client/build")));
@@ -30,17 +57,41 @@ if (!isDev && cluster.isMaster) {
   if (process.env.NODE_ENV === "development") {
     const cors = require("cors");
     app.use(cors());
-    console.log("dev server");
   }
 
   // Answer API requests.
   app.get("/api", (req, res) => {
     res.set("Content-Type", "application/json");
-    res.send('{"message":"Hello from my API you bitch!"}');
+    res.send({ message: "hello from my backend API!" });
   });
 
   app.get("/api/login", (req, res) => {
-    res.redirect("http://www.google.ca");
+    // Set auth request state cookie
+    const state = generateRandomString(16);
+    res.cookie(stateKey, state);
+
+    // Request authorization
+    const scope = "user-read-private user-read-email";
+
+    res.redirect(
+      spotify_auth_url +
+        queryString.stringify({
+          response_type: "code",
+          client_id: client_id,
+          scope: scope,
+          redirect_uri: redirect_uri,
+          state: state,
+        })
+    );
+  });
+
+  app.get("/api/callback", (req, res) => {
+    const code = req.query.code || null;
+    const state = req.query.state || null;
+    const storedState = req.cookies ? req.cookies[stateKey] : null;
+
+    res.set("Content-Type", "application/json");
+    res.send({ state: state, code: code, storedState: storedState });
   });
 
   // All remaining requests return the React app, so it can handle routing.
