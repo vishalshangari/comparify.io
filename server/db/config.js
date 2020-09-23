62a2f1c327db39ff23d05a5f91c26d33273e9462
blackmambabryant <vishal.shangari@gmail.com>
2020-09-22 22:36:31 -0700

    move all firebase config into env vars

--- server/db/config.js
+++ server/db/config.js
@@ -1,14 +1,12 @@
 module.exports = {
-  type: "service_account",
-  project_id: "spotify-compare",
+  type: process.env.FIREBASE_KEY_TYPE,
+  project_id: process.env.FIREBASE_PROJECT_ID,
   private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
   private_key: JSON.parse(process.env.FIREBASE_PRIVATE_KEY),
-  client_email:
-    "firebase-adminsdk-c7062@spotify-compare.iam.gserviceaccount.com",
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
   client_id: process.env.FIREBASE_CLIENT_ID,
-  auth_uri: "https://accounts.google.com/o/oauth2/auth",
-  token_uri: "https://oauth2.googleapis.com/token",
-  auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
-  client_x509_cert_url:
-    "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-c7062%40spotify-compare.iam.gserviceaccount.com",
+  auth_uri: process.env.FIREBASE_AUTH_URI,
+  token_uri: process.env.FIREBASE_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER,
+  client_x509_cert_url: process.env.FIREBASE_CLIENT_X509,
 };
